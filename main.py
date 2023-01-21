2240a45d7b0e53f64a13446231941a91f0004ce1
Victor Prado <vmrpadob@gmail.com>
2023-01-21 16:59:42 -0400

    validation with query parameters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Body
+from typing import Optional
+from fastapi import FastAPI, Body, Query
 from Models.Person import Person
 app = FastAPI()
 
@@ -13,3 +14,10 @@ def home():
 @app.post("/person")
 def create_person(person: Person = Body(...)):
     return person
+
+@app.get("/person/details")
+def show_person(
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    age: Optional[int] = Query(None, le=100, ge=1)
+):
+    return {name: age}
